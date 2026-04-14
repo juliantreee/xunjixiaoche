fb13b237a2f4781be1fe8aa93422cbe21c2d650a
Tree <52529283+juliantreee@users.noreply.github.com>
2026-04-14 11:22:06 +0800

    improve lib pid
    make pid great!

--- mcu program/PID.h
+++ mcu program/PID.h
@@ -3,8 +3,9 @@
 
 typedef struct PID PID;
 
-PID* pid_create(double kp, double ki, double kd); //PID对象创建函数
-double pid_step(PID *in,double value);  //单次pid函数
-void pid_set_target(PID *in, double target);
+PID* pid_create(double kp, double ki, double kd, double Ctlmax, double Ctlmin,double dt); //PID对象创建函数
+double pid_step(PID *in,double value,double target);  //单次pid函数
+void pid_set_limit(PID *in, double max, double min); //设置输出限制
+void pid_destroy(PID *in);//释放内存
 
 #endif
