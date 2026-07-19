c66ff236aa8351f8af5ccbe202c96e58a8be93e3
JIN41212 <qianjin20070412@163.com>
2026-07-19 14:11:02 +0800

    报错修改

--- mcu program/uart.h
+++ mcu program/uart.h
@@ -1,11 +1,13 @@
 //
 // Created by qianj on 2026/5/3.
 //
-
+/*
 #ifndef MCU_PROGRAM_UART_H
 #define MCU_PROGRAM_UART_H
 #include "string.h"
 #include "stdio.h"
 void Set_CurrentUART(uint8_t UART_type);
 static UART_Regs *current_uart;
-#endif //MCU_PROGRAM_UART_H
+#endif 
+*/
+//MCU_PROGRAM_UART_H
